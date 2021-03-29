8be5a26ec23e66b394fcfc91520a94e1e3a3dcaf
Jack Harris <jackaharrisii@gmail.com>
2021-03-29 17:01:14 -0400

    stubbed out the first couple projects

---
+++ OdeToFood.Data.Tests/RestaurantShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OdeToFood.Data.Tests
+{
+    public class RestaurantShould
+    {
+
+        [Fact]
+        public void Doublethink_2plus2_equals5()  //UnitOfWork_InitialCondition_ExpectedResult
+        {
+            //Arrange
+            int expected = 5;
+            int actual = 2 + 2;
+            //Act
+            actual = Doublethink(actual);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        public int Doublethink(int input)
+        {
+            return input + 1;
+        }
+
+    }
+}
